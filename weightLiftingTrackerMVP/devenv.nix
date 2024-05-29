eb22e25e8713391de8f1dc5f942c4d3f9dfa9e65
aaronfllr <aaronfllr@gmail.com>
2024-05-28 22:28:21 -0400

    First commit

--- weightLiftingTrackerMVP/devenv.nix
+++ weightLiftingTrackerMVP/devenv.nix
@@ -1,22 +1,41 @@
 { pkgs, lib, config, inputs, ... }:
 
+let
+  # Configure which Android tools we'll need (mostly the recommended ones)
+  sdk = (import inputs.android-nixpkgs { }).sdk (sdkPkgs:
+    with sdkPkgs; [
+      build-tools-30-0-3
+      build-tools-33-0-0
+      cmdline-tools-latest
+      emulator
+      patcher-v4
+      platform-tools
+      platforms-android-33
+      system-images-android-32-google-apis-x86-64
+    ]);
+in
 {
   # https://devenv.sh/basics/
-  env.GREET = "Aaron";
+  env.GREET = "Aaron, happy coding!!";
 
   # https://devenv.sh/packages/
   packages = with pkgs; [
     postgresql_16
+    android-studio
+    watchman
   ];
 
   env = {
     PRISMA_QUERY_ENGINE_LIBRARY = "${pkgs.prisma-engines}/lib/libquery_engine.node";
     PRISMA_QUERY_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/query-engine";
     PRISMA_SCHEMA_ENGINE_BINARY = "${pkgs.prisma-engines}/bin/schema-engine";
+    ANDROID_HOME = "/home/aaron/Android/Sdk";
+    ANDROID_SDK_ROOT = "${pkgs.android-tools}/bin";
   };
 
+
   # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
+  scripts.hello.exec = "echo Hey $GREET";
   
   scripts = {
     prisma.exec = "npx prisma $@";
@@ -25,6 +44,7 @@
   enterShell = ''
     hello
     git --version
+    export PATH="${sdk}/bin:$PATH"
   '';
 
   # https://devenv.sh/tests/
@@ -32,7 +52,7 @@
     echo "Running tests"
     git --version | grep "2.42.0"
   '';
-
+  
   # https://devenv.sh/services/
   # services.postgres.enable = true;
 
